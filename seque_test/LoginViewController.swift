cb6d1c76b3e500cecfc5190cbee66a5b7a0f8345
GSMag <gsmagx@me.com>
2019-01-11 14:17:50 +0100

    update

---
+++ seque_test/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seque_test
+//
+//  Created by Alex on 11.01.19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ seque_test/MyCitiesCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyCitiesCell.swift
+//  seque_test
+//
+//  Created by Alex on 11.01.19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class MyCitiesCell: UITableViewCell {
+
+    @IBOutlet weak var myNamesCities: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
