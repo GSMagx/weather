25b9f5bb9f4af18a7616f2f1ed247cf3bced6ae3
GSMagx <gsmagx@me.com>
2019-01-12 09:36:57 +0100

    +1

--- seque_test/AllCitiesController.swift
+++ seque_test/AllCitiesController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class AllCitiesController: UITableViewController {
-    
-    
-    
+
     let cities = [
         "Moskow",
         "Krasnoyarsk",
@@ -20,12 +18,9 @@ class AllCitiesController: UITableViewController {
         "Hamburg",
         "Berlin"
     ]
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-  
     }
 
     // MARK: - Table view data source
@@ -39,8 +34,6 @@ class AllCitiesController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return cities.count
     }
-
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AllCitiesCell", for: indexPath) as! AllCitiesCell
 
@@ -50,51 +43,4 @@ class AllCitiesController: UITableViewController {
         
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- seque_test/MyCitiesController.swift
+++ seque_test/MyCitiesController.swift
@@ -15,20 +15,11 @@ class MyCitiesController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return cities.count
@@ -39,72 +30,36 @@ class MyCitiesController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCitiesCell", for: indexPath) as! MyCitiesCell
         
         let myCities = cities[indexPath.row]
-        cell.myNamesCities.text = myCities
+        cell.myNamesCities?.text = myCities
 
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func addCity(segue: UIStoryboardSegue) {
         
+        // Проверяем идентификатор перехода, чтобы убедиться, что это нужный
         if segue.identifier == "addCity" {
+            // Получаем ссылку на контроллер, с которого осуществлен переход
             let allCitiesController = segue.source as! AllCitiesController
-            print("ссылка на контроллер = \(allCitiesController)")
             
-            if  let indexPath = allCitiesController.tableView.indexPathForSelectedRow {
-                print("индекс ячейки = \(indexPath)")
+            // Получаем индекс выделенной ячейки
+            if let indexPath = allCitiesController.tableView.indexPathForSelectedRow {
+                // Получаем город по индексу
                 let city = allCitiesController.cities[indexPath.row]
-                cities.append(city)
-                tableView.reloadData()
+                
+                // Проверяем, что такого города нет в списке
+                if !cities.contains(city) {
+                    // Добавляем город в список выбранных
+                    cities.append(city)
+                    // Обновляем таблицу
+                    tableView.reloadData()
+                }
             }
         }
-        
-        
     }
+
+    
+    
 
 }
